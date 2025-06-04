6099bf6d48a1c5e2f2d90afbb649fabd719d50e7
alex <alexandermorrison16@gmail.com>
2025-06-04 11:02:51 +1000

    fixing frontend

--- resources/views/weather/index.blade.php
+++ resources/views/weather/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Weather Forecast</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body class="bg-gray-100">
